050a94104b6db96b39499d702c33b47a12e4d6f5
Cecilio-Cauich <celiocatu@gmail.com>
2022-10-13 22:18:37 -0500

    usando api key

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
 console.log("Hello word");
 
 const btn_refresh = document.querySelector("#btn-refreshpage");
+const btn_clean = document.querySelector("#btn-clean");
+const contenedorprin = document.querySelector(".main-containter");
+
+
 function reload() {
   reload = location.reload();
 }
 
-const btn_clean = document.querySelector("#btn-clean");
-
 btn_refresh.addEventListener("click", reload, false);
 btn_clean.addEventListener("click", cleancontainer, false);
 
-const contenedorprin = document.querySelector(".main-containter");
-
-
 async function reload() {
   let cantidadgatos = document.querySelector("#input-num").value
-  const res = await fetch("https://api.thecatapi.com/v1/images/search?limit="+cantidadgatos);
+
+  const res = await fetch('https://api.thecatapi.com/v1/images/search?limit='+cantidadgatos+'&api_key=live_Y011HAuLgyQFPeUMLJ193i0XxMEp2bE6LT5MKfAtpc9lX1p7e8O59jO28cAdIdnX');
   const data = await res.json();
-   
-  console.log("Después del await"+data)
   for (let index = 0; index < cantidadgatos; index++) {
     const inimagen = document.createElement("img");
     inimagen.src = data[index].url;
-    console.log(data[index].url)
-    inimagen.setAttribute("class", "foto");
+    inimagen.setAttribute("class","foto");
     contenedorprin.append(inimagen);
+
   }
 }
 
