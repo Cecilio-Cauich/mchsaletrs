66a470828c19e634b29426e1ec7f62105bb13ab0
Cecilio-Cauich <celiocatu@gmail.com>
2022-10-16 14:02:23 -0500

    usando get

--- main.js
+++ main.js
@@ -1,46 +1,81 @@
-console.log("Hello word");
-
 const btn_refresh = document.querySelector("#btn-refreshpage");
 const btn_clean = document.querySelector("#btn-clean");
 const contenedorprin = document.querySelector("#cont-content");
+const spanError = document.querySelector('#error');
 
-
-function reload() {
-  reload = location.reload();
-}
-
-btn_refresh.addEventListener("click", reload, false);
+btn_refresh.addEventListener("click", loadRandomMichis, false);
 btn_clean.addEventListener("click", cleancontainer, false);
 
-async function reload() {
+async function loadRandomMichis() {
   let cantidadgatos = document.querySelector("#input-num").value
 
   const res = await fetch('https://api.thecatapi.com/v1/images/search?limit='+cantidadgatos+'&api_key=live_Y011HAuLgyQFPeUMLJ193i0XxMEp2bE6LT5MKfAtpc9lX1p7e8O59jO28cAdIdnX');
   const data = await res.json();
-  for (let index = 0; index < cantidadgatos; index++) {
-    const inimagen = document.createElement("img");
-    const contenedorBtnImg = document.createElement("div");
-    const btnFavorito = document.createElement("button","Guardar");
   
-    inimagen.setAttribute("class","foto");
-    contenedorBtnImg.setAttribute("class","cont-img-btn")
-    btnFavorito.setAttribute("class","btn-fav");
-    btnFavorito.type='button';
-    btnFavorito.innerHTML='Guardar';
+  if(res.status !== 200){
+    spanError.innerHTML = "Hubo un error: "+res.status+data.message;
+  }else{
+    for (let index = 0; index < cantidadgatos; index++) {
+      const inimagen = document.createElement("img");
+      const contenedorBtnImg = document.createElement("div");
+      const btnFavourite = document.createElement("button","Guardar");
     
-    inimagen.src = data[index].url;
-  
-    contenedorBtnImg.append(inimagen);
-    contenedorBtnImg.append(btnFavorito);
-    contenedorprin.append(contenedorBtnImg);
+      inimagen.setAttribute("class","foto");
+      contenedorBtnImg.setAttribute("class","cont-img-btn")
+      btnFavourite.setAttribute("class","btn-fav");
+      btnFavourite.type='button';
+      btnFavourite.innerHTML='Guardar';
+      
+      inimagen.src = data[index].url;
+    
+      contenedorBtnImg.append(inimagen);
+      contenedorBtnImg.append(btnFavourite);
+      contenedorprin.append(contenedorBtnImg);
+    }
   }
+
+
+  
+}
+
+async function loadFavoritesMichis() {
+  console.log("favoritos");
+
+  const res = await fetch('https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_Y011HAuLgyQFPeUMLJ193i0XxMEp2bE6LT5MKfAtpc9lX1p7e8O59jO28cAdIdnX');
+  const data = await res.json();
+  console.log(data);
+  // for (let index = 0; index < cantidadgatos; index++) {
+  //   const inimagen = document.createElement("img");
+  //   const contenedorBtnImg = document.createElement("div");
+  //   const btnFavorito = document.createElement("button","Guardar");
+  
+  //   inimagen.setAttribute("class","foto");
+  //   contenedorBtnImg.setAttribute("class","cont-img-btn")
+  //   btnFavorito.setAttribute("class","btn-fav");
+  //   btnFavorito.type='button';
+  //   btnFavorito.innerHTML='Guardar';
+    
+  //   inimagen.src = data[index].url;
+  
+  //   contenedorBtnImg.append(inimagen);
+  //   contenedorBtnImg.append(btnFavorito);
+  //   contenedorprin.append(contenedorBtnImg);
+  // }
   
 }
 
 function cleancontainer() {
   const elementosaeliminar = document.querySelectorAll('.cont-img-btn');
-  for(let elemento of elementosaeliminar){
-    contenedorprin.removeChild(elemento);
+
+  if(res.status !== 200){
+    spanError.innerHTML = "Hubo un error: "+res.status;
+  }else{
+    for(let elemento of elementosaeliminar){
+      contenedorprin.removeChild(elemento);
+    }
   }
 }
+
+loadFavoritesMichis();
+
 
